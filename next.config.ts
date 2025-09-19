dc9ead0b392804c8c5951b499d1129c23443649a
gaona233 <1064193183@qq.com>
2025-09-19 10:44:43 +0800

    github page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+// next.config.js
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig: NextConfig = {
   /* config options here */
+output: "export",
+  assetPrefix: isProd ? '/demo-it-consultis/' : '', // 替换为你的仓库名,
   images: {
     remotePatterns: [
       {
